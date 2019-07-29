e3a84984685aa1deb9bee03cad5f2d77d1c1a09c
krishnapande2000 <53431690+krishnapande2000@users.noreply.github.com>
2019-07-29 16:56:10 +0530

    Add files via upload

---
+++ lab1sub.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1;
+    cin>>s1;
+    string s2;
+    cin>>s2;
+    char a='0';
+    int p=0,t;
+    if(s1.length()<s2.length())
+    {
+       string temp=s1;
+       s1=s2;
+       s2=temp;
+       cout<<"-";
+    }
+  if(s1.length()==s2.length())
+    {
+       for(int i=0;i<s1.length();i++)
+       {
+          if(s1[i]<s2[i])
+          {
+          string temp=s1;
+          s1=s2;
+          s2=temp;
+          cout<<"-";
+          break;
+          }
+          }
+    }
+    while(s2.length()<s1.length())
+    {
+       s2.insert(0,"0",1);
+    }   
+    
+    char result[s1.length()];
+    //result[s1.length()]='\0';
+    for(int i=s1.length()-1; i>=0  ;i--)
+    { 
+         
+        t= (int)(s1[i]-'0') - (int)(s2[i] -'0') + p;
+        p=0;
+        if(t<0)
+        {
+            p=-1;
+            t+=10;
+        }
+        result[s1.length()-i-1]=(char) (t + (int)a ) ;
+     }
+     
+     for(int i=1;i<=s1.length();i++)
+        cout<<result[s1.length()-i];
+     cout<<endl;   
+     
+     return 0;
+}   
