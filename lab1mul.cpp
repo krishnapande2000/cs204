cb43b067bc848b525567db0699440830f202c92b
krishnapande2000 <53431690+krishnapande2000@users.noreply.github.com>
2019-08-02 16:50:35 +0530

    Add files via upload

---
+++ lab1mul.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+string add(string s1,string s2)
+{
+    char a='0';
+    int p=0,t;
+    if(s1.length()<s2.length())
+    {
+       string temp=s1;
+       s1=s2;
+       s2=temp;
+    }
+    while(s2.length()<s1.length())
+    {
+       s2.insert(0,"0",1);
+    }   
+    
+    string result="";                                            
+    for(int i=s1.length()-1; i>=0  ;i--)
+    { 
+         
+        t= (int)(s1[i]-'0') + (int)(s2[i] -'0') + p;
+        p=t/10;
+        t=t%10;
+        result.push_back((char) (t + (int)a )) ;
+     }
+     if(p==1) result.push_back('1');
+      
+      reverse(result.begin(), result.end());
+     return result;
+}
+string mul1(string s, char a)
+{ 
+    int k=(int)(a-'0');
+   
+    int p=0,t; 
+    string result="";
+    for(int i=s.length()-1; i>=0  ;i--)
+    { 
+         
+        t= ((int)(s[i]-'0'))*k + p;
+        p=t/10;
+        t=t%10;
+        result.push_back((char) (t + (int)('0'))) ;
+        
+     }
+     reverse(result.begin(), result.end());
+     
+     return result;
+ }
+    
+int main()
+{
+    string s1;
+    cin>>s1;
+    string s2;
+    cin>>s2;
+    string res="0";
+    for(int i=s2.length()-1; i>=0  ;i--)
+    {
+         string mul=mul1(s1,s2[i]);
+         mul.append((s2.length()-1-i),'0'); 
+         res= add(res,mul);
+    }
+    cout<<res<<endl;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
